16b4008cda71b7879dbe70e3ee8a0deeab22d6a2
Aaron J. Brown <aaron.joseph.brown@me.com>
2025-03-12 11:06:28 -0500

    Initial Commit

---
+++ Link/LinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkApp.swift
+//  Link
+//
+//  Created by Aaron Brown on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
